e7c9efb972ca5078470b0ab92d4b43c1f36873f6
Diego Andai <diego.andai@platan.us>
2021-03-17 10:35:04 -0300

    feat(components): add ScreenContainer

---
+++ components/ScreenContainer.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+const ScreenContainer = styled.SafeAreaView`
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default ScreenContainer;
