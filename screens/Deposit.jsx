39bcea2bc772704640e45b4edc950d734034e53a
Diego Andai <diego.andai@platan.us>
2021-03-17 10:35:31 -0300

    feat(screens): add basic tab screens

---
+++ screens/Deposit.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AppText from '../components/AppText.jsx';
+import ScreenContainer from '../components/ScreenContainer.jsx';
+
+export default function Deposit() {
+  return (
+    <ScreenContainer>
+      <AppText>Deposit</AppText>
+    </ScreenContainer>
+  );
+}
