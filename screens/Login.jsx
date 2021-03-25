da539503780e1c507fac47fee761d569ba43e747
Diego Andai <diego.andai@platan.us>
2021-03-25 01:31:18 -0300

    Refactor: use TextInput and SessionScreen on Login

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,56 +1,47 @@
-/* eslint-disable react/style-prop-object */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { StatusBar } from 'expo-status-bar';
 import I18n from 'i18n-js';
+import { useNavigation } from '@react-navigation/native';
 
 import { COLORS, StyleUtils } from '../utils/styles';
 
 import Button from '../components/Button.jsx';
-import ScrollableScreen from '../components/ScrollableScreen.jsx';
-
-const greenPigSource = require('../assets/green-pig.png');
+import SessionScreen from '../components/SessionScreen.jsx';
+import TouchableText from '../components/TouchableText.jsx';
+import TextInput from '../components/TextInput.jsx';
 
 export default function Login({ login }) {
+  const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <SafeAreaView>
-      <StatusBar style="light" />
-      <ScrollableScreen>
-        <Container>
-          <SizedImage
-            source={greenPigSource}
-            resizeMode="contain"
-          />
-          <FormContainer>
-            <StyledInput
-              onChangeText={setEmail}
-              value={email}
-              autoCapitalize="none"
-              placeholder={I18n.t(['user', 'email'])}
-              placeholderTextColor={COLORS.white}
-              selectionColor={COLORS.white}
-            />
-            <StyledInput
-              onChangeText={setPassword}
-              value={password}
-              autoCapitalize="none"
-              placeholder={I18n.t(['user', 'password'])}
-              placeholderTextColor={COLORS.white}
-              selectionColor={COLORS.white}
-              secureTextEntry
-            />
-          </FormContainer>
-          <SpacedButton
-            title={I18n.t(['session', 'login'])}
-            onPress={() => login(email, password)}
-          />
-        </Container>
-      </ScrollableScreen>
-    </SafeAreaView>
+    <SessionScreen>
+      <FormContainer>
+        <SpacedInput
+          onChangeText={setEmail}
+          value={email}
+          placeholder={I18n.t('user.email')}
+        />
+        <SpacedInput
+          onChangeText={setPassword}
+          value={password}
+          placeholder={I18n.t('user.password')}
+          secureTextEntry
+        />
+      </FormContainer>
+      <SpacedButton
+        title={I18n.t('session.login')}
+        onPress={() => login(email, password)}
+      />
+      <SpacedSignupLink
+        textColor={COLORS.green}
+        onPress={() => navigation.navigate('Signup')}
+      >
+        { I18n.t('session.signup') }
+      </SpacedSignupLink>
+    </SessionScreen>
   );
 }
 
@@ -58,37 +49,19 @@ Login.propTypes = {
   login: PropTypes.func.isRequired,
 };
 
-const SafeAreaView = styled.SafeAreaView`
-  background-color: ${COLORS.blue};
-  flex: 1;
-`;
-
-const Container = styled.View`
-  align-items: center;
-  flex: 1;
-  justify-content: center;
-  ${StyleUtils.padded('lg')}
-`;
-
 const FormContainer = styled.View`
   ${StyleUtils.spacedTop('lg')};
   width: 100%;
-`;
-
-const SizedImage = styled.Image`
-  height: 15%;
-  max-width: 100%;
 `;
 
 const SpacedButton = styled(Button)`
   ${StyleUtils.spacedTop('lg')};
 `;
 
-const StyledInput = styled.TextInput`
-  border-bottom-width: 1px;
-  border-color: white;
-  color: white;
-  ${StyleUtils.padded('sm')};
+const SpacedSignupLink = styled(TouchableText)`
   ${StyleUtils.spacedTop()};
-  width: 100%;
+`;
+
+const SpacedInput = styled(TextInput)`
+  ${StyleUtils.spacedTop()};
 `;
