d94a37ba293b793ee8a2366f64a4057cef66cf1d
Diego Andai <diego.andai@platan.us>
2021-03-31 17:29:24 -0300

    Feat: add Bank screen

---
+++ components/BankField.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, View } from 'react-native';
+import Clipboard from 'expo-clipboard';
+import I18n from 'i18n-js';
+
+import {
+  COLORS, StyleUtils,
+} from '../utils/styles';
+
+import AppText from './AppText.jsx';
+
+const COPIED_CONFIRMATION_DURATION = 1000;
+
+export default function BankField({ name, value, style }) {
+  const [message, setMessage] = useState(I18n.t('screens.bank.copy'));
+  const [highlightMessage, setHighlightMessage] = useState(false);
+
+  function copyValue() {
+    Clipboard.setString(value);
+    setMessage(I18n.t('screens.bank.copied'));
+    setHighlightMessage(true);
+    setTimeout(() => {
+      setMessage(I18n.t('screens.bank.copy'));
+      setHighlightMessage(false);
+    }, COPIED_CONFIRMATION_DURATION);
+  }
+  return (
+    <TouchableOpacity
+      onPress={copyValue}
+    >
+      <Container style={style}>
+        <View>
+          <NameText>
+            {name}
+          </NameText>
+          <ValueText>
+            {value}
+          </ValueText>
+        </View>
+        <View>
+          <MessageText
+            highlight={highlightMessage}
+          >
+            { message }
+          </MessageText>
+        </View>
+      </Container>
+    </TouchableOpacity>
+  );
+}
+
+BankField.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  style: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.object),
+    PropTypes.objectOf(
+      PropTypes.oneOfType(
+        [PropTypes.number, PropTypes.string],
+      ),
+    ),
+  ]),
+};
+
+BankField.defaultProps = {
+  style: {},
+};
+
+const Container = styled.View`
+  align-items: center;
+  background-color: ${COLORS.blue};
+  flex-direction: row;
+  justify-content: space-between;
+  ${StyleUtils.paddedX('sm')}
+  ${StyleUtils.paddedY('xs')}
+  ${StyleUtils.rounded('sm')}
+`;
+
+const NameText = styled(AppText)`
+  color: ${COLORS.lightBlue};
+  ${StyleUtils.fontSize('sm')}
+`;
+
+const ValueText = styled(AppText)`
+  color: white;
+`;
+
+const MessageText = styled(AppText)`
+  color: ${({ highlight }) => (highlight ? 'white' : COLORS.lightBlue)};
+  ${StyleUtils.fontSize('sm')}
+`;
--- locales/es.js
+++ locales/es.js
@@ -31,6 +31,20 @@ const es = {
       topTickets: 'Estos son tus top %{count}',
       none: 'No tienes boletos',
     },
+    bank: {
+      depositTitle: 'Obtén boletos',
+      depositInstructions: 'Deposítanos a la siguiente cuenta, te enviaremos un mail cuando recibamos tu depósito',
+      withdrawTitle: 'Retira tu dinero',
+      withdrawInstructions: 'Envía tus datos bancarios y monto a retirar al siguiente email',
+      name: 'Nombre',
+      rut: 'RUT',
+      bankName: 'Banco',
+      accountType: 'Tipo de cuenta',
+      accountNumber: 'Número de cuenta',
+      email: 'Correo',
+      copy: 'Copiar',
+      copied: '¡Copiado!',
+    },
   },
 };
 
