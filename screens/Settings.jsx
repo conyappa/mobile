63cd377e34696db2a2795fc63fdddb4a365ce029
Daniel Leal <dlleal@uc.cl>
2021-04-06 16:38:55 -0400

    feat: hardcode notion URL

--- screens/Settings.jsx
+++ screens/Settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
+import { Linking } from 'react-native';
 
 import I18n from 'i18n-js';
 import { COLORS, StyleUtils } from '../utils/styles';
@@ -16,6 +17,12 @@ export default function Settings({ logout }) {
       <RulesText>
         { I18n.t('misc.rules') }
       </RulesText>
+      <MoreInfoText
+        onPress={() => Linking.openURL('https://www.notion.so/conyappa/Informaci-n-Con-Yappa-f82daa1f76284e588c707ff6eb2a35e7')}
+        textColor={COLORS.lightBlue}
+      >
+        Toca acá para más información
+      </MoreInfoText>
       <TitleText>{ I18n.t('screens.settings.logoutTitle') }</TitleText>
       <SpacedLogout
         onPress={logout}
@@ -33,8 +40,11 @@ Settings.propTypes = {
 
 const RulesText = styled(AppText)`
   ${StyleUtils.spacedX('lg')}
-  ${StyleUtils.spacedBottom('xl')}
   text-align: justify;
+`;
+
+const MoreInfoText = styled(TouchableText)`
+  ${StyleUtils.spacedBottom('xl')}
 `;
 
 const TitleText = styled(AppText)`
