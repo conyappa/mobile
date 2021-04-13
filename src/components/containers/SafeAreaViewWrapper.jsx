1cc4509140278890c9e842573d916900c73b4416
Daniel Leal <37158127+daleal@users.noreply.github.com>
2021-04-13 10:58:50 -0400

    Add safe view (#42)
    * Create a safe area component
    
    * Wrap all the containers into a safe area component
    
    * Remove a bit of top-padding from padded container

---
+++ src/components/containers/SafeAreaViewWrapper.jsx
@@ -0,0 +1,8 @@
+import { SafeAreaView } from 'react-native';
+import styled from 'styled-components/native';
+
+const SafeAreaViewWrapper = styled(SafeAreaView)`
+  flex: 1;
+`;
+
+export default SafeAreaViewWrapper;
