434327d6ff4daeeca2e4ffa325926335176cf35b
Daniel Leal <37158127+daleal@users.noreply.github.com>
2021-04-13 13:11:37 -0400

    Create build workflow (#41)
    * feat: add a script to build the production version
    
    * feat: add triggered workflow to build the app
    
    * feat: add apple team id to the command and specify for the console not to wait for the build
    
    * feat: add environmental variable for apple team id on the workflow file
    
    * chore: change the name of the workflow
    
    * Add documentation

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Builds the production version of the application.
+# sh ./scripts/build.sh <OS>
+# Requires the environmental variables:
+# - EXPO_TOKEN: Access Token for the expo account to use (when not authenticated)
+# - APPLE_TEAM_ID: ID of the Apple Team (when building for iOS)
+
+if [ -z $1 ]; then
+    echo "An operating system (\"ios\", \"android\") must be passed as a parameter."
+    exit 1
+fi
+
+if [ $1 = "ios" ]; then
+  expo build:ios \
+    -t archive \
+    --release-channel production \
+    --team-id $APPLE_TEAM_ID \
+    --skip-credentials-check \
+    --no-publish \
+    --no-wait
+elif [ $1 = "android" ]; then
+  expo build:android \
+    -t app-bundle \
+    --release-channel production \
+    --no-publish \
+    --no-wait
+else
+  echo "The parameter passed doesn't correspond to a valid operating system (\"ios\", \"android\")."
+  exit 1
+fi
