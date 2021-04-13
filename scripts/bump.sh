36e99ad7d5fc17f3fb34d1b8292aac3c4622dc42
Daniel Leal <37158127+daleal@users.noreply.github.com>
2021-04-13 14:09:23 -0400

    Fix bump script (#45)
    * fix: reorder git commands and execute everything from the repository base
    
    * fix: reorder git commands and execute everything from the repository base for the build bump script too
    
    * fix: don't tag git versions
    
    * fix: ammed lying comment
    
    * Apply suggestions from code review
    
    Co-authored-by: Ariel Martínez Silberstein <amartinez5@uc.cl>
    
    Co-authored-by: Ariel Martínez Silberstein <amartinez5@uc.cl>

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -10,13 +10,15 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-# Bump up package.json version and get new version
-yarn version --$1
+# Get base repository folder
+SCRIPTS_FOLDER=$(cd $(dirname $0) && pwd)
+BASE_REPOSITORY_FOLDER=$(dirname $SCRIPTS_FOLDER)
 
-# Get new version
+# Bump up package.json version and get new version
+yarn version --no-git-tag-version --$1
 NEW_VERSION=$(yarn --silent getversion)
 
 # Commit changes into release branch
-git add package.json &&
 git checkout -b release/prepare-$NEW_VERSION &&
-git commit --message "Prepare $NEW_VERSION release"
+git add $BASE_REPOSITORY_FOLDER/package.json &&
+git commit --message "chore: prepare $NEW_VERSION release"
