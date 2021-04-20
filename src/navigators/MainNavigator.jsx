a01c374b457e777044f30dbd20fef423629f24a4
Ariel Martínez Silberstein <ariel@martinezs.dev>
2021-04-20 11:48:15 -0400

    Request push notifications permission (#55)
    * feat: ask notifications permission
    
    * style: remove console logs
    
    * style: rename vars
    
    * fix: set android notification channel

--- src/navigators/MainNavigator.jsx
+++ src/navigators/MainNavigator.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Platform } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
 
 import useSession from '@/hooks/useSession';
 
@@ -10,6 +13,7 @@ export default function MainNavigator() {
   const {
     login, logout, isLogged, userId, checkedLocal,
   } = useSession();
+  const [, setExpoPushToken] = useState(null);
 
   useEffect(() => {
     async function hideSplash() {
@@ -17,9 +21,36 @@ export default function MainNavigator() {
         await SplashScreen.hideAsync();
       }
     }
-
     hideSplash();
   }, [checkedLocal]);
+
+  async function registerForPushNotifications() {
+    if (Constants.isDevice) {
+      const { status: currentStatus } = await Notifications.getPermissionsAsync();
+      let status = currentStatus;
+
+      if (status !== 'granted') {
+        const { status: newStatus } = await Notifications.requestPermissionsAsync();
+        status = newStatus;
+      }
+
+      if (status === 'granted') {
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        setExpoPushToken(token);
+
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+      }
+    }
+  }
+
+  useEffect(() => { registerForPushNotifications(); }, []);
 
   if (!checkedLocal) {
     return null;
