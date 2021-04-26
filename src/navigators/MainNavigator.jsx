1a22a9ef9087c183cc5e06684374668e57fa452f
Diego Andai <diego.andai@platan.us>
2021-04-26 12:39:41 -0400

    Feat: ask notif permissions and register device

--- src/navigators/MainNavigator.jsx
+++ src/navigators/MainNavigator.jsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Platform } from 'react-native';
+import React, { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
 
 import useSession from '@/hooks/useSession';
 
@@ -13,7 +10,6 @@ export default function MainNavigator() {
   const {
     login, logout, isLogged, userId, checkedLocal,
   } = useSession();
-  const [, setExpoPushToken] = useState(null);
 
   useEffect(() => {
     async function hideSplash() {
@@ -23,34 +19,6 @@ export default function MainNavigator() {
     }
     hideSplash();
   }, [checkedLocal]);
-
-  async function registerForPushNotifications() {
-    if (Constants.isDevice) {
-      const { status: currentStatus } = await Notifications.getPermissionsAsync();
-      let status = currentStatus;
-
-      if (status !== 'granted') {
-        const { status: newStatus } = await Notifications.requestPermissionsAsync();
-        status = newStatus;
-      }
-
-      if (status === 'granted') {
-        const token = (await Notifications.getExpoPushTokenAsync()).data;
-        setExpoPushToken(token);
-
-        if (Platform.OS === 'android') {
-          Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-          });
-        }
-      }
-    }
-  }
-
-  useEffect(() => { registerForPushNotifications(); }, []);
 
   if (!checkedLocal) {
     return null;
